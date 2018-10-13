ce2178fddc35f54b76b942898376b69afe4fd644
Raleigh Littles <raleighlittles@gmail.com>
2018-10-13 12:35:44 -0700

    Added more comments; started draft of add_node()

--- chapter_5/linked_digit_list.cpp
+++ chapter_5/linked_digit_list.cpp
@@ -8,7 +8,9 @@
 
 Number::Number(int starting_number)
 {
-    // Starting with a specified number, seperate it into groups of 3 starting from the left -- then add those 3-digit-long sequences as nodes in the list.
+    // Starting with a specified number, seperate it into groups of X digits starting from the left -- then add those X-digit-long sequences as nodes in the list, where X is the maximum digit length.
+
+    // In the case where the group of digits is less than X-characters long, prepend 0s to the digit to ensure all digit sequences in the list have the same digit length
 
     std::string starting_number_as_string = std::to_string(starting_number);
 
@@ -44,6 +46,9 @@ std::string Number::to_string()
 
 void Number::add_node(int number) {
 
+    std::string number_as_string = std::to_string(number);
+
+    if number_as_string.length()
 
 }
 
