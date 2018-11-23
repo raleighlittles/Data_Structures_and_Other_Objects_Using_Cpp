19ab1228b4165a25d4a706c89203fcb0b1a1f5b5
Raleigh Littles III <raleighlittles@gmail.com>
2018-11-23 00:29:08 -0800

    Finished problem 9 of chapter 9. Now onto chapter 10!

--- chapter_9/chapter_9_projects.hpp
+++ chapter_9/chapter_9_projects.hpp
@@ -2,10 +2,9 @@
 // Created by raleigh on 11/11/18.
 //
 
+#include <sstream>
 
-
-namespace RecursiveThinking
-{
+namespace RecursiveThinking {
 
     /* Problem description, problem 6
      *
@@ -23,8 +22,7 @@ and write a recursive C++ function that computes
 the value of the formula. Embed the function in a
 program and test it.
      */
-    float recursive_combination_formula(int n, int r)
-    {
+    float recursive_combination_formula(int n, int r) {
 
         /* Remember that the C(n, r) formula is the same as the binomial coefficient. Then, we can use Pascal's Rule:
     C(n, r) = C(n-1, r) + C(n-1, r-1) to create a recursive implementation. */
@@ -32,31 +30,58 @@ program and test it.
         // this is by definition of the binomial coefficient.
         // there are 0 ways to choose a sample of r items from a collection of n unless r is a non-negative number
         // similarly, it is impossible to choose a set of r elements from n if r is larger than n
-        if ((r < 0) or (r > n))
-        {
+        if ((r < 0) or (r > n)) {
             return 0;
         }
 
         // there is only 1 way to choose r items out of n if you wish to choose no items, and that is not choosing any items at all
         // similarly, if there are no elements to even choose from, there is obviously only way to choose (not choosing)
-        if ((r == 0) or (n <=1 ))
-        {
+        if ((r == 0) or (n <= 1)) {
             return 1;
         }
 
-        return (recursive_combination_formula(n-1, r) + recursive_combination_formula(n-1, r-1));
+        return (recursive_combination_formula(n - 1, r) + recursive_combination_formula(n - 1, r - 1));
 
     }
 
+    // std::string does have a way to create repeated characters, but it does NOT work with strings. E.g.:
+    // std::string("a", 4) works, and will return "aaaa", but you cannot replace "a" with an std::string-like type
+    std::string operator*(const std::string &str, std::size_t times) {
+        std::stringstream stream;
+        for (size_t i = 0; i < times; i++) {
+            stream << str;
+        }
+        return stream.str();
+    }
 
-     /* Problem description, Problem 9 */
-     /*
-      * Examine this pattern of asterisks and blanks,
+
+    std::string recursive_function(uint8_t left_margin, uint8_t length) {
+        if (length == 0) {
+            return "";
+        } else {
+            return (recursive_function(left_margin, length / 2) + std::string("  ") * left_margin +
+                    std::string("* ") * length + "\n" +
+                    recursive_function(left_margin + length / 2, length / 2));
+        }
+
+    }
+
+    /* Problem description, Problem 9 */
+    /*
+     * Examine this pattern of asterisks and blanks,
 and write a recursive function that can gen-
 erate exactly this pattern:
-      */
-    std::string generate_asterisk_pattern()
-    {
+     */
+    std::string generate_asterisk_pattern() {
+        // Hint: If you're having trouble understanding the pattern, write out how many asterisks appear on each (horizontal)
+        // line.
+
+        return recursive_function(0, 8);
 
     }
+
+
 }
+
+
+
--- chapter_9/main.cpp
+++ chapter_9/main.cpp
@@ -5,4 +5,6 @@
 int main() {
 
     std::cout << RecursiveThinking::recursive_combination_formula(10, 6) << std::endl;
+
+    std::cout << RecursiveThinking::generate_asterisk_pattern() << std::endl;
 }
