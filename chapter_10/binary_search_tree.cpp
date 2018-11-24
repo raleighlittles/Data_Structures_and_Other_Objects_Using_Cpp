ea23b81fcd534d3de2ee96e410103c6f7bdcdf55
Raleigh Littles III <raleighlittles@gmail.com>
2018-11-23 21:57:47 -0800

    added constructor

--- chapter_10/binary_search_tree.cpp
+++ chapter_10/binary_search_tree.cpp
@@ -9,13 +9,19 @@ class Node
 {
 private:
     Type node_value;
-    std::unique_ptr<Node> left;
-    std::unique_ptr<Node> right;
+    std::unique_ptr<Node> left_node;
+    std::unique_ptr<Node> right_node;
 
-    https://gist.github.com/mgechev/5911348
+    //https://gist.github.com/mgechev/5911348
 
-
-
+    Node(Type value, Node<Type> left, Node<Type> right)
+    :
+    node_value(value),
+    left_node(left),
+    right_node(right)
+    {
+        // empty constructur
+    }
 
 };
 
