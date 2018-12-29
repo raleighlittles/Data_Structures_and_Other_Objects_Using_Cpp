c3ca3159603a8285aaaafb219b728086b7dbabbb
Raleigh Littles <raleighlittles@gmail.com>
2018-12-28 22:51:30 -0800

    First commit for problem #5 (dictionary) of Chapter 12.
    Added the word list for Q (since its the shortest) and got the basic file structure in place.

---
+++ chapter_12/dictionary.h
@@ -0,0 +1,14 @@
+//
+// Created by raleigh on 12/28/18.
+//
+
+#ifndef CHAPTER_12_DICTIONARY_H
+
+
+class WordDictionary {
+
+};
+
+#define CHAPTER_12_DICTIONARY_H
+
+#endif //CHAPTER_12_DICTIONARY_H
